0d6a0fba68b0a2c7f17a8d5d436ba1c6c6b68d8c
matsuzaki <matsuzaki@codia.co.jp>
2024-03-05 18:21:20 +0900

    MongoDBユーザー登録機能

--- app/utils/schemaModels.js
+++ app/utils/schemaModels.js
@@ -10,4 +10,23 @@ const ItemSchema = new Schema({
   emaill: String,
 });
 
-export const ItemModel = mongoose.models.Item || mongoose.model("Item", ItemSchema);
+const UserScheme = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export const ItemModel =
+  mongoose.models.Item || mongoose.model("Item", ItemSchema);
+
+export const UserModel = mongoose.models.User || mongoose.model("User", UserScheme);
