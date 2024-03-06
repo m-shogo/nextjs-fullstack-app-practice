b64cfc4e5722cbe51ec25d859b3ca936fb52e0d7
matsuzaki <matsuzaki@codia.co.jp>
2024-03-06 12:33:53 +0900

    JSON Web Token（ JWT）でトークン発行 https://jwt.io/

--- app/api/user/login/route.js
+++ app/api/user/login/route.js
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import { SignJWT } from "jose";
 import connectDB from "../../../utils/database";
 import { UserModel } from "../../../utils/schemaModels";
 
@@ -6,18 +7,38 @@ export async function POST(request) {
   const reqBody = await request.json();
 
   try {
-    await connectDB;
+    await connectDB();
     const savedUserData = await UserModel.findOne({ email: reqBody.email });
     if (savedUserData) {
+      // ユーザーデータが存在する場合の処理
       if (reqBody.password === savedUserData.password) {
-        return NextResponse.json({ message: "ログイン成功" });
+        // パスワードが正しい場合の処理
+        const secretKey = new TextEncoder().encode(
+          "nextjs-fullstack-app-practice"
+        );
+        const payload = {
+          email: reqBody.email,
+        };
+
+        const token = await new SignJWT(payload)
+          .setProtectedHeader({ alg: "HS256" })
+          .setExpirationTime("4d")
+          .sign(secretKey);
+
+        console.log(token);
+
+        return NextResponse.json({ message: "ログイン成功", token: token });
       } else {
+        // パスワードが間違っている場合の処理
         return NextResponse.json({
           message: "ログイン失敗:パスワードが間違っています",
         });
       }
     } else {
-      return NextResponse.json({ message: "ログイン失敗:ユーザー登録をしてください" });
+      // ユーザーデータが存在しない場合の処理
+      return NextResponse.json({
+        message: "ログイン失敗:ユーザー登録をしてください",
+      });
     }
   } catch (err) {
     return NextResponse.json({ message: "ログイン失敗" });
