a7eadb7057d2197fff7bc54097e415cfb81dd74e
matsuzaki <matsuzaki@codia.co.jp>
2024-03-06 12:01:01 +0900

    MongoDBログイン機能

--- app/api/user/login/route.js
+++ app/api/user/login/route.js
@@ -1,13 +1,25 @@
 import { NextResponse } from "next/server";
-import connectDB from "../../../../utils/database";
-import { ItemModel } from "../../../../utils/schemaModels";
+import connectDB from "../../../utils/database";
+import { UserModel } from "../../../utils/schemaModels";
 
-export async function DELETE(request, context) {
+export async function POST(request) {
+  const reqBody = await request.json();
+
   try {
     await connectDB;
-    await ItemModel.deleteOne({ _id: context.params.id });
-    return NextResponse.json({ message: "アイテム削除成功" });
+    const savedUserData = await UserModel.findOne({ email: reqBody.email });
+    if (savedUserData) {
+      if (reqBody.password === savedUserData.password) {
+        return NextResponse.json({ message: "ログイン成功" });
+      } else {
+        return NextResponse.json({
+          message: "ログイン失敗:パスワードが間違っています",
+        });
+      }
+    } else {
+      return NextResponse.json({ message: "ログイン失敗:ユーザー登録をしてください" });
+    }
   } catch (err) {
-    return NextResponse.json({ message: "アイテム削除失敗" });
+    return NextResponse.json({ message: "ログイン失敗" });
   }
 }
