5040700178a6d8ff0919ac31e4d94ad3e1bfb889
matsuzaki <matsuzaki@codia.co.jp>
2024-03-06 14:40:33 +0900

    誰がログインしているかを判定する機能

--- app/api/item/delete/[id]/route.js
+++ app/api/item/delete/[id]/route.js
@@ -3,10 +3,16 @@ import connectDB from "../../../../utils/database";
 import { ItemModel } from "../../../../utils/schemaModels";
 
 export async function DELETE(request, context) {
+  const reqBody = await request.json();
   try {
-    await connectDB;
-    await ItemModel.deleteOne({ _id: context.params.id });
-    return NextResponse.json({ message: "アイテム削除成功" });
+    await connectDB();
+    const singleItem = await ItemModel.findById(context.params.id);
+    if (singleItem.email === reqBody.email) {
+      await ItemModel.deleteOne({ _id: context.params.id });
+      return NextResponse.json({ message: "アイテム削除成功" });
+    } else {
+      return NextResponse.json({ message: "他の人が作成したアイテムです" });
+    }
   } catch (err) {
     return NextResponse.json({ message: "アイテム削除失敗" });
   }
--- app/api/item/update/[id]/route.js
+++ app/api/item/update/[id]/route.js
@@ -2,12 +2,18 @@ import { NextResponse } from "next/server";
 import connectDB from "../../../../utils/database";
 import { ItemModel } from "../../../../utils/schemaModels";
 
-export async function PUT(request,context) {
+export async function PUT(request, context) {
   const reqBody = await request.json();
   try {
-    await connectDB;
-    await ItemModel.updateOne({_id: context.params.id},reqBody);
-    return NextResponse.json({ message: "アイテム編集成功" });
+    await connectDB();
+    const singleItem = await ItemModel.findById(context.params.id);
+    if(singleItem.email === reqBody.email){
+      await ItemModel.updateOne({ _id: context.params.id }, reqBody);
+      return NextResponse.json({ message: "アイテム編集成功" });
+    } else{
+      return NextResponse.json({ message: "他の人が作成したアイテムです" });
+    }
+
   } catch (err) {
     return NextResponse.json({ message: "アイテム編集失敗" });
   }
