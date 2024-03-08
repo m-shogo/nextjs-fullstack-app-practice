6f47e9a9cf24a44de4b9210cfc4640d3f7c6d090
matsuzaki <matsuzaki@codia.co.jp>
2024-03-08 15:25:57 +0900

    【フロント】アイテム 一覧/個別 ページ

--- app/page.js
+++ app/page.js
@@ -1,7 +1,35 @@
-const ReadAllItems = () => {
+import Link from "next/link";
+import Image from "next/image";
+
+const getAllItems = async () => {
+  const response = await fetch("http://localhost:3002/api/item/readall", {
+    cache: "no-cache",
+  });
+  const jsonData = await response.json();
+  const allItems = jsonData.allItems;
+  return allItems;
+};
+
+const ReadAllItems = async () => {
+  const allItems = await getAllItems();
   return (
     <div>
-      <h1>こんにちは</h1>
+      {allItems.map((item) => (
+        <Link href={`/item/readsingle/${item._id}`} key={item._id}>
+          <Image
+            src={item.image}
+            width={750}
+            height={500}
+            alt="item-image"
+            priority
+          />
+          <div key={item._id}>
+            <h2>{item.price}</h2>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
