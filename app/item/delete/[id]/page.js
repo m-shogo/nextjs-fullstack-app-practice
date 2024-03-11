84e790f17edf2a9087ad1da590e1ea2c9b8146fe
matsuzaki <matsuzaki@codia.co.jp>
2024-03-11 17:42:37 +0900

    【フロント】アイテム データ削除 ページ

---
+++ app/item/delete/[id]/page.js
@@ -0,0 +1,68 @@
+"use client";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+
+const DeleteItem = (context) => {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
+  const [description, setDescription] = useState("");
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    const getSingleItem = async (id) => {
+      const response = await fetch(
+        `http://localhost:3002/api/item/readsingle/${id}`,
+        { cache: "no-store" }
+      );
+      const jsonData = await response.json();
+      const singleItem = jsonData.singleItem;
+
+      setTitle(singleItem.title);
+      setPrice(singleItem.price);
+      setImage(singleItem.image);
+      setDescription(singleItem.description);
+      setEmail(singleItem.email);
+    };
+    getSingleItem(context.params.id);
+  }, [context]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `http://localhost:3002/api/item/delete/${context.params.id}`,
+        {
+          method: "DELETE",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+          body: JSON.stringify({
+            email: "m.shogo.0409@gmail.com",
+          }),
+        }
+      );
+      const jsonData = await response.json();
+      alert(jsonData.message);
+    } catch (err) {
+      alert("アイテム削除失敗");
+    }
+  };
+
+  return (
+    <div>
+      <h1>アイテム削除</h1>
+      <form onSubmit={handleSubmit}>
+        <h2>{title}</h2>
+        <Image src={image} width={750} height={500} alt="item-image" priority />
+        <h3>￥{price}</h3>
+        <p>{description}</p>
+        <button>削除</button>
+      </form>
+    </div>
+  );
+};
+
+export default DeleteItem;
--- app/item/readsingle/[id]/page.js
+++ app/item/readsingle/[id]/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const getSingleItem = async (id) => {
   const response = await fetch(
@@ -15,7 +16,7 @@ const ReadSingleItem = async (context) => {
   const singleItem = await getSingleItem(context.params.id);
   return (
     <div>
-      <div key={singleItem._id}>
+      <div>
         <Image
           src={singleItem.image}
           width={750}
@@ -23,10 +24,15 @@ const ReadSingleItem = async (context) => {
           alt="item-image"
           priority
         />
-        <div key={singleItem._id}>
-          <h2>{singleItem.price}</h2>
-          <h3>{singleItem.title}</h3>
-          <p>{singleItem.description}</p>
+      </div>
+      <div>
+        <h1>{singleItem.title}</h1>
+        <h2>¥{singleItem.price}</h2>
+        <hr />
+        <p>{singleItem.description}</p>
+        <div>
+          <Link href={`/item/update/${singleItem._id}`}>アイテム編集</Link>
+          <Link href={`/item/delete/${singleItem._id}`}>アイテム削除</Link>
         </div>
       </div>
     </div>
