d984d80be416fdba321bf5201e091f8fe29797ca
matsuzaki <matsuzaki@codia.co.jp>
2024-03-12 17:07:52 +0900

    【フロント】Google font複数設定方法

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,25 @@
 import "./globals.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
+import { Zen_Kaku_Gothic_New,Montserrat } from "next/font/google";
+
+const zen_Kaku_Gothic_New = Zen_Kaku_Gothic_New({
+  weight: ["400", "500"],
+  subsets: ["latin"],
+  variable: "--font-zen_Kaku_Gothic_New",
+  display: "swap",
+});
+
+const montserrat = Montserrat({
+  weight: ["400", "500"],
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+  display: "swap",
+});
 
 const RootLayout = ({ children }) => {
   return (
-    <html lang="jp">
+    <html lang="jp" className={`${zen_Kaku_Gothic_New.variable} ${montserrat.variable}`}>
       <body>
         <Header />
         {children}
